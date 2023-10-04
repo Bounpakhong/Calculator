09071b4a478fcc3481ac369b33160bb554b918be
Bounpakhong <p.bounpakhong@gmail.com>
2023-10-04 17:37:33 +0700

    updated code

---
+++ calculator.js
@@ -0,0 +1,29 @@
+var zero = document.getElementById("zero");
+var one = document.getElementById("one");
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+var four = document.getElementById("four");
+var five = document.getElementById("five");
+var six = document.getElementById("six");
+var seven = document.getElementById("seven");
+var eight = document.getElementById("eight");
+var nine = document.getElementById("nine");
+var plus = document.getElementById("plus");
+var minus = document.getElementById("minus");
+var times = document.getElementById("times");
+var div = document.getElementById("div");
+var equato = document.getElementById("equato");
+var point = document.getElementById("point");
+var bracket = document.getElementById("bracket");
+var percent = document.getElementById("percent");
+var cls = document.getElementById("cls");
+var bs = document.getElementById("bs");
+
+var eq = document.getElementsByClassName("equation")[0];
+var res = document.getElementsByClassName("result")[0];
+
+zero.onclick = function () {
+    //eq.value += "0";
+    //eq.focus();
+    
+}
