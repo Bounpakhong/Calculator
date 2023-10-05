4ac9411ae313c74fe1acd5220a8b03b24405dcb1
Bounpakhong <p.bounpakhong@gmail.com>
2023-10-05 17:18:11 +0700

    update

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+//#region "var button elements"
 var zero = document.getElementById("zero");
 var one = document.getElementById("one");
 var two = document.getElementById("two");
@@ -18,11 +19,58 @@ var bracket = document.getElementById("bracket");
 var percent = document.getElementById("percent");
 var cls = document.getElementById("cls");
 var bs = document.getElementById("bs");
+//#endregion
 
+//#region "var display elements"
 var eq = document.getElementsByClassName("equation")[0];
 var res = document.getElementsByClassName("result")[0];
+//#endregion
 
-zero.onclick = function () {
-    eq.value += "0";
+//#region "button's event"
+zero.onclick = () => numbers("0");
+one.onclick = () => numbers("1");
+two.onclick = () => numbers("2");
+three.onclick = () => numbers("3");
+four.onclick = () => numbers("4");
+five.onclick = () => numbers("5");
+six.onclick = () => numbers("6");
+seven.onclick = () => numbers("7");
+eight.onclick = () => numbers("8");
+nine.onclick = () => numbers("9");
+point.onclick = () => operator(".");
+plus.onclick = () => operator("+");
+minus.onclick = () => operator("-");
+times.onclick = () => operator("*");
+div.onclick = () => operator("/");
+//#endregion
+
+//#region buttons' function
+function numbers(n) {
+    if (eq.value == "0") {
+        eq.value = n;
+    } else {
+        eq.value += n;
+    }
     eq.focus();
 }
+function operator(o) {
+    let end = eq.value.length - 1;
+    if (o != ".") {
+        if (/[+\-*/.]/.test(eq.value[end])) {
+            eq.value = eq.value.slice(0, end) + o;
+        } else if (eq.value == "" || eq.value == "0") {
+            eq.value = "0" + o;
+        } else {
+            eq.value += o;
+        }
+    } else {
+        if (/[+\-*/.]/.test(eq.value[end])) {
+            eq.value = eq.value.slice(0, end) + o;
+        } else if(eq.value == "0" || eq.value == "") {
+            eq.value = "0.";
+        } else {
+            eq.value += ".";
+        }
+    }
+}
+//#endregion
